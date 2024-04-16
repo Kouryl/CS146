d3d0edfe77444e6ddf79b0a8b11b60a22716bb7b
Alan <139190527+Kouryl@users.noreply.github.com>
2024-04-15 18:35:52 -0700

    code update

--- Labs/Lab6/Lab6.java
+++ Labs/Lab6/Lab6.java
@@ -1,19 +1,25 @@
+import java.util.*;
 public class Lab6 {
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
         boolean finish = true;
-        int[] in = new int[numCourses];
+        ArrayList<Integer> in = new ArrayList<>();
 
-        for (int i = 0; i < in.length; i++) { //initialize all in-degrees to 0
-            in[i] = 0;
+        for (int i = 0; i < numCourses; i++) { //initialize all in-degrees to 0
+            in.add(0);
         }
 
         for (int i = 0; i < prerequisites.length; i++) { //find in-degrees of all courses
-            in[prerequisites[i][0]] += 1;
+            in.set(prerequisites[i][0], in.get(prerequisites[i][0]) + 1);
         }
 
         for (int i = 0; i < prerequisites.length; i++) {
-            if (in[prerequisites[i][1]] != 0) {
-                //if pre req has incoming edges, then we can't finish.
+            if (in.get(prerequisites[i][1]) == 0) {
+                in.set(prerequisites[i][0], in.get(prerequisites[i][0]) - 1); //decrement in degrees
+            }
+        }
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            if (in.get(prerequisites[i][1]) != 0) {
                 finish = false;
             }
         }
--- Labs/Lab6/Lab6Test.java
+++ Labs/Lab6/Lab6Test.java
@@ -10,5 +10,15 @@ public class Lab6Test {
         assertEquals(true, Lab6.canFinish(2, test));
         int[][] test1 = {{1,0},{0,1}};
         assertEquals(false, Lab6.canFinish(2, test1));
+        int[][] test2 = {{1,0}, {2,0}, {0,1}};
+        assertEquals(false, Lab6.canFinish(3, test2));
+        int[][] test3 = {{1,0}, {2,0}};
+        assertEquals(true, Lab6.canFinish(3, test3));
+        int[][] test4 = {{1,0}, {2,1}, {3,2}};
+        assertEquals(true, Lab6.canFinish(4, test4));
+        int[][] test5 = {{1,0}, {2,1}, {3,2}, {2, 3}};
+        assertEquals(false, Lab6.canFinish(4, test5));
+        int[][] test6 = {{0,1}, {1,2}, {2,3}, {3,0}};
+        assertEquals(false, Lab6.canFinish(4, test6));
     }
 }
