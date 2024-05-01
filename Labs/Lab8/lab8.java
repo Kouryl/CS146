00f175daeece5f20cf9856251aee5ac5817e1f4c
Alan <139190527+Kouryl@users.noreply.github.com>
2024-04-30 20:47:09 -0700

    Update lab8.java

--- Labs/Lab8/lab8.java
+++ Labs/Lab8/lab8.java
@@ -5,15 +5,15 @@ public class lab8 {
         PriorityQueue<Integer> q = new PriorityQueue<>();
         Stack<Integer> s  = new Stack<>();
 
-        for (int i = 0; i < coins.length; i++) { //nlogn to sort
+        for (int i = 0; i < coins.length; i++) {
             q.offer(coins[i]);
         }
 
-        for (int i = 0; i < coins.length; i++) {//constant time to add to stack
+        for (int i = 0; i < coins.length; i++) {
             s.push(q.poll());
         }
 
-        while (amount > 0) { //should be linear time (the size of the stack)
+        while (amount > 0) { 
             if (s.size() == 0 && amount > 0) {
                 coinAmount = -1;
                 break;
